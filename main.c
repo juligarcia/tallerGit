0bfbc0c4b82308bed320b1bb6d297ff9d367c203
Julian Garcia <julian.garcia@widergy.com>
2020-07-03 18:58:41 -0300

    implemente suma bien

--- main.c
+++ main.c
@@ -2,11 +2,14 @@
 
 #define HELLO_WORLD "Hello world!"
 
-int suma(a, b){
-	return (a + b);
-}
+int suma(int, int);
 
 int main(){
 	printf("%s\n", HELLO_WORLD);
+	printf("La suma entre (2+1) da: %d\n", suma(2, 1));
 	return 0;
+}
+
+int suma(int a, int b){
+	return a + b;
 }
